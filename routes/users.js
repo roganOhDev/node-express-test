532f2dba2d3b510bdb474b1b9848087c1d67920d
roganOhDev <fdscbjdcnhd@gmail.com>
2023-07-13 00:07:20 -0700

    make comment for thr new error example

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,8 @@ import {ApiError} from "../errors/apiError.js";
 const userRouter = Router();
 
 userRouter.get('/users', function (req, res, next) {
-    throw new ApiError(400, 'This is a bad request.');
-    // res.send('respond with a resource');
+    // throw new ApiError(400, 'This is a bad request.');
+    res.send('respond with a resource');
 });
 
 export default userRouter;
